503bd2742c3edc7f7d65d3bd2e1a6761013761e9
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-09-22 20:42:47 +0530

    Create navbar and me jsx file

---
+++ src/Component/Me.jsx
@@ -0,0 +1,16 @@
+function Me() {
+    return(
+        <div className="flex ml-48 mt-28">
+        <div class="h-200 w-200  ">
+            <img class="rounded-full" src="./src/Image/ItsMe-removebg-preview.png" alt="" />
+        </div>
+        <div>
+            <h1 className="">
+                I am Bishnupada Kamila<br></br>
+                Skilled in Programming Languages
+            </h1>
+        </div>
+        </div>
+    )
+}
+export default Me
