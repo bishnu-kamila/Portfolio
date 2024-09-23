98fa216c57cd7468be2240ba385e49ea9207190f
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-09-23 23:35:14 +0530

    Install Router Dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Contact from './Component/Contact.jsx'
+import Rout from './Rout.jsx'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Contact/>
+    <Rout/>
   </StrictMode>,
 )
