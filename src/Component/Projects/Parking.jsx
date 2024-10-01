fff8079ced09a73dd49492d45cac14e4d6bd9b85
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-10-01 17:18:46 +0530

    Adding new Project

---
+++ src/Component/Projects/Parking.jsx
@@ -0,0 +1,10 @@
+const Parking = ()=>{
+    return(
+        <>
+            <div>
+                <h1>ParkingManagementSystem</h1>
+            </div>
+        </>
+    )
+}
+export default Parking;
