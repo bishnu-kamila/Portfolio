6d9a610ade5fcfc9d42cbba0910351cfe0914851
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-10-02 23:29:54 +0530

    Update content

--- src/Component/Projects/Parking.jsx
+++ src/Component/Projects/Parking.jsx
@@ -3,6 +3,12 @@ const Parking = ()=>{
         <>
             <div>
                 <h1>ParkingManagementSystem</h1>
+                <div>
+                    Make easy and safe Parking in online mode In this project we have the Admin , User and Security. Admin
+                    have the Access to allow user account and allow for Vehicle Registration . User Create account and Modify
+                    Profile then ask permission for Parking Security have the access to allow the Vehicle at the time of Vehicle
+                    Registration and Give the permission to the user for parking in every day
+                </div>
             </div>
         </>
     )
