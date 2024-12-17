d80f4e2db1aa8f1d3f2e7cad041d61374ea8fdb2
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-12-17 16:35:47 +0530

    Update Project.jsx
    Update

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,12 +1,14 @@
 import Footer from "./Footer"
 import Projects from "./Projects/Projects"
 import UPMS from "./Projects/UPMS"
+import UPMS from "./Projects/Parking"
 
 const Project = ()=>{
     return(
         <>
             <Projects/>
             <UPMS/>
+            <Parking/>
         </>
     )
 }
