1fa49879719b45c0a74dc8fcbd95dc2c9c1f64cc
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-19 23:11:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,6 @@ import Me from './Component/Me';
 import React from 'react';
 import Home from './Component/Home';
 import { Outlet } from 'react-router-dom';
-import Footer from './Component/Footer';
-import Technology from './Component/Technology';
-import Education from './Component/Education';
-import Projects from './Component/Projects/Projects';
-import UPMS from './Component/Projects/UPMS';
-import Parking from './Component/Projects/Parking';
 
 function App() {
   const [count, setCount] = useState(0);
