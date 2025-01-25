2f6efcbada6c3f0aaeb0c35e536dec8946f44b98
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-25 20:03:34 +0530

    update

--- src/Component/Education.jsx
+++ src/Component/Education.jsx
@@ -3,28 +3,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Education = () => {
   return (
-    <div className="w-full px-4 mt-20 ml-[200px]">
+    <div className="w-full px-4 mt-16 md:mt-20">
       {/* Section for Education */}
-      <div className="text-white font-bold italic text-3xl sm:text-4xl md:text-5xl">
+      <div className="text-white font-bold italic text-2xl sm:text-3xl md:text-4xl">
+        {/* Bachelor's Degree Section */}
         <div
           data-aos="fade-down"
-          className="flex items-center justify-center md:justify-start space-x-4"
+          className="flex flex-wrap items-center justify-center md:justify-start space-x-2 sm:space-x-4"
         >
-          <FontAwesomeIcon icon={faGraduationCap} size="2x" />
-          <h1 className="ml-2 text-lg sm:text-xl">BSc : </h1>
-          <h1 className="text-lg sm:text-xl">FakirMohon University, Balasore</h1>
+          <FontAwesomeIcon
+            icon={faGraduationCap}
+            className="text-xl sm:text-2xl md:text-3xl"
+          />
+          <h1 className="text-base sm:text-lg md:text-xl">BSc:</h1>
+          <h1 className="text-base sm:text-lg md:text-xl">
+            FakirMohon University, Balasore
+          </h1>
         </div>
-        <div
-  data-aos="fade-up"
-  className="ml-[30px] flex items-center justify-center md:justify-start mt-8 space-x-4"
->
-  <img className="h-12 w-12 sm:h-16 sm:w-16" src="./Master.png" alt="MCA" />
-  <h1 className="text-lg sm:text-xl">MCA :</h1>
-  <h1 className="text-lg sm:text-xl ml-2">
-    Centurion University of Technology and Management, Bhubaneswar
-  </h1>
-</div>
 
+        {/* Master's Degree Section */}
+        <div
+          data-aos="fade-up"
+          className="flex flex-wrap items-center justify-center md:justify-start mt-6 sm:mt-8 space-x-2 sm:space-x-4"
+        >
+          <img
+            className="h-10 w-10 sm:h-12 sm:w-12 md:h-16 md:w-16 object-contain"
+            src="./Master.png"
+            alt="MCA"
+          />
+          <h1 className="text-base sm:text-lg md:text-xl">MCA:</h1>
+          <h1 className="text-base sm:text-lg md:text-xl">
+            Centurion University of Technology and Management, Bhubaneswar
+          </h1>
+        </div>
       </div>
     </div>
   );
